f56f35f6a033823326c7a9e7a6b93be9017a2b79
lemmyc <mrburgundy2911@gmail.com>
2022-12-12 00:03:32 +0700

    remove dummy input

--- assets/scripts/api.js
+++ assets/scripts/api.js
@@ -1,16 +1,3 @@
-input = {
-  "carat": "2",
-  "cut": "2",
-  "color": "2",
-  "clarity": "2",
-  "depth": "2",
-  "table": "2",
-  "x": "2",
-  "y": "2",
-  "z": "2"
-}
-
-
 async function postData(url = '', data = {}) {
   const response = await fetch(url, {
     method: 'POST',
